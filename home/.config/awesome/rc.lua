956d2cd574555339d870f71f736c1b5757ff8420
renyuneyun <renyuneyun@gmail.com>
2014-12-20 20:23:25 +0800

    add conky && awesome autostart conky

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -87,6 +87,17 @@ for s = 1, screen.count() do
 end
 -- }}}
 
+function run_once(cmd)
+  findme = cmd
+  firstspace = cmd:find(" ")
+  if firstspace then
+    findme = cmd:sub(0, firstspace-1)
+  end
+  awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
+end
+
+run_once("conky")
+
 -- {{{ Menu
 -- Create a laucher widget and a main menu
 
