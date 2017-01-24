fac9aa28f43edb03ba8b5c46d3b70f8f35e85608
renyuneyun <renyuneyun@gmail.com>
2017-01-24 21:41:53 +0000

    awesome/deploy.sh: correctly handle deployment

--- home/.config/awesome/deploy.sh
+++ home/.config/awesome/deploy.sh
@@ -3,8 +3,31 @@
 srcdir=$1
 dstdir=$2
 
-mkdir $dstdir
-for file in autorun.lua rc.lua panels.lua freedesktop; do
-	ln -sr $file $dstdir/$file
+if [ ! -d $dstdir ]; then
+	mkdir $dstdir
+fi
+
+except=(deploy.sh third_party)
+
+function can_deploy {
+	local file="$1"
+	for arr in ${except[@]}; do
+		if [ "$file" = "$arr" ]; then
+			return 1 #false
+		fi
+	done
+	if [[ "$file" =~ \.bak$ ]] || [[ $file =~ \.old$ ]]; then
+		return 1
+	fi
+	return 0 #true
+}
+
+for file in `ls`; do
+	if can_deploy $file; then
+		if [ -f $dstdir/$file ]; then
+			rm $dstdir/$file
+		fi
+		ln -sr $file $dstdir/$file
+	fi
 done
 
